ba0615145dafc816d6439833c4b5417597bba0ed
Dharmendra Kumar <114353361+dharmendr963@users.noreply.github.com>
2023-05-30 07:08:08 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,51 +1,9 @@
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const flash = require('connect-flash');
-const session = require('express-session');
-
 const app = express();
+const router = require('../routes/index'); 
 
-// Passport Config
-require('./config/passport')(passport);
-
-// DB Config
-const db = require('./config/keys').mongoURI;
-
-// Connect to MongoDB
-mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true ,useUnifiedTopology: true}
-  )
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
-
-// EJS
-app.use(expressLayouts);
-app.set('view engine', 'ejs');
-
-// Express body parser
-app.use(express.urlencoded({ extended: true }));
-
-// Express session
-app.use(
-  session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-  })
-);
-
-// Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Connect flash
-app.use(flash());
-
-// Global variables
+//Router Middlewares
+app.use(express.json());
 app.use(function(req, res, next) {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
@@ -53,10 +11,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-// Routes
-app.use('/', require('./routes/index.js'));
-app.use('/users', require('./routes/users.js'));
 
-const PORT = process.env.PORT || 5000;
+app.use('/api/v1/', router);
 
-app.listen(PORT, console.log(`Server running on  ${PORT}`));
+module.exports = app;
