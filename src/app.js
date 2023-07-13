372a6e6763afb2563c72b65390637e6d6cc923a3
Dharmendra Kumar <114353361+dharmendr963@users.noreply.github.com>
2023-07-12 18:51:35 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const app = express();
-const router = require('../routes/index'); 
 
-//Router Middlewares
+// Import the User model/schema
+const { User } = require('./models');
+
+// Middleware to parse JSON in the request body
 app.use(express.json());
-app.use(function(req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-  next();
+
+// User registration route
+app.post('/api/v1/user/register', async (req, res) => {
+  const { name, email, password, DOB } = req.body;
+
+  try {
+    // Create a new user using the User model
+    const user = new User({
+      name,
+      email,
+      password,
+      DOB
+    });
+  
+    // Save the user to the database
+    await user.save();
+  
+    // Return the user's _id if registration is successful
+    res.status(200).json({ _id: user._id });
+  } catch (error) {
+    // Return the error message with a 404 status if there was an error in registration
+    res.status(404).json({ error: error.message });
+  }
 });
 
-
-app.use('/api/v1/', router);
-
-module.exports = app;
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
